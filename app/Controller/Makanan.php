0e472db0d497ef10f312b3c804c338343ec89bb4
Adit-oscar <tiyaa0048@gmail.com>
2023-01-09 19:59:05 +0800

    menambahkan fitur untuk menambahkan makanan

--- app/Controller/Makanan.php
+++ app/Controller/Makanan.php
@@ -19,7 +19,6 @@ class Makanan extends Controller
     $this->helper->controller_not_found('fail', 'Controller / Method Not Found!', 404);
   }
 
-
   public function getMakanan()
   {
 
@@ -61,8 +60,7 @@ class Makanan extends Controller
         $data = [
           "id" => $dataMakanan['id'],
           "nama" => $dataMakanan['nama_makanan'],
-          "harga" => $dataMakanan['harga_makanan'],
-          // "gambar" => '../resto-api/assets/img/makanan/' . $dataMakanan['foto']
+          "harga" => $dataMakanan['harga_makanan']
         ];
 
         $this->helper->response_getMakanan("success", $data, 200);
@@ -75,182 +73,39 @@ class Makanan extends Controller
     }
   }
 
-  // function addMakanan()
-  // {
-  //   global $db;
+  function addMakanan()
+  {
+    global $db;
 
-  //   $nama_makanan = isset($_POST['nama_makanan']) ? $_POST['nama_makanan'] : '';
-  //   $harga_makanan = isset($_POST['harga_makanan']) ? $_POST['harga_makanan'] : '';
+    $nama_makanan = isset($_POST['nama_makanan']) ? $_POST['nama_makanan'] : '';
+    $harga_makanan = isset($_POST['harga_makanan']) ? $_POST['harga_makanan'] : '';
 
-  //   // konfigurasi untuk mendapatkan foto
-  //   if (isset($_FILES['foto']['name'])) {
-  //     $foto_tmp = isset($_FILES['foto']['tmp_name']) ? $_FILES['foto']['tmp_name'] : '';
-  //     $foto_name = isset($_FILES['foto']['name']) ? $_FILES['foto']['name'] : '';
-  //   } else {
-  //     $foto_name = isset($_POST['foto']) ? $_POST['foto'] : '';
-  //     $ekstensi = isset($_POST['ekstensi']) ? $_POST['ekstensi'] : '';
-  //     $foto = base64_decode($foto_name);
-  //   }
+    if ($nama_makanan !== '' && $harga_makanan !== '') {
 
-  //   if ($nama_makanan !== '' && $harga_makanan !== '' && $foto_name !== '') {
+      $sql = "INSERT INTO makanan VALUES ('', '$nama_makanan', '$harga_makanan')";
+      $query = $db->query($sql);
 
-  //     $new_names = rename_foto($foto_name, $nama_makanan);
+      if ($query === true) {
 
-  //     $sql = "INSERT INTO makanan VALUES ('', '$nama_makanan', '$harga_makanan', '$new_names')";
-  //     $query = $db->query($sql);
-  //     if ($query === true) {
+        $response = $this->helper->response_addMakanan("success", "Data berhasil ditambahkan", 201);
+        return $response;
+      } else {
 
-  //       file_put_contents('../resto-api/assets/img/makanan/' . $new_names . $ekstensi, $foto);
+        $response = $this->helper->response_failAddMakanan("fail", "Data gagal ditambahkan", 400);
+        return $response;
+      }
+    } elseif ($nama_makanan == '') {
 
-  //       // memindahkan gambar kedalam path direktori penyimpanan
-  //       move_uploaded_file($foto_tmp, '../resto-api/assets/img/makanan/' . $new_names);
+      $response = $this->helper->response_failAddMakanan("fail", "Data gagal ditambahkan, field nama_makanan tidak boleh kosong", 400);
+      return $response;
+    } elseif ($harga_makanan == '') {
 
-  //       $response = new Response("success", "", "Data berhasil ditambahkan", 201);
-  //       return $response->response_addMakanan();
-  //     } else {
+      $response = $this->helper->response_failAddMakanan("fail", "Data gagal ditambahkan, field harga_makanan tidak boleh kosong", 400);
+      return $response;
+    } else {
 
-  //       $response = new Response("fail", "", "Data gagal ditammbahkan", 400);
-  //       return $response->response_failAddMakanan();
-  //     }
-  //   } elseif ($nama_makanan == '') {
-
-  //     $response = new Response("fail", "", "Field nama tidak boleh kosong", 400);
-  //     return $response->response_failAddMakanan();
-  //   } elseif ($harga_makanan == '') {
-
-  //     $response = new Response("fail", "", "Field harga tidak boleh kosong", 400);
-  //     return $response->response_failAddMakanan();
-  //   } elseif ($foto_name == '') {
-
-  //     $response = new Response("fail", "", "Field foto tidak boleh kosong", 400);
-  //     return $response->response_failAddMakanan();
-  //   } else {
-
-  //     $response = new Response("fail", "", "Gagal menambahkan makanan, semua field harus diisi!", 400);
-  //     return $response->response_field_empty();
-  //   }
-  // }
-
-  // function editMakanan($id)
-  // {
-  //   global $db;
-
-  //   $nama_makanan = isset($_POST['nama_makanan']) ? $_POST['nama_makanan'] : '';
-  //   $harga_makanan = isset($_POST['harga_makanan']) ? $_POST['harga_makanan'] : '';
-
-  //   // konfigurasi untuk mendapatkan foto
-  //   $foto_tmp = $_FILES['foto']['tmp_name'];
-  //   $foto_name = $_FILES['foto']['name'];
-
-  //   if ($foto_name !== '') {
-
-
-  //     $sql = "SELECT * FROM makanan WHERE id='$id'";
-  //     $data = $db->query($sql);
-
-  //     if ($data->num_rows == 1) {
-
-  //       if ($nama_makanan !== '' && $harga_makanan !== '' && $foto_name !== '') {
-
-  //         $new_names = rename_foto($foto_name, $nama_makanan);
-
-  //         // menghapus foto lama
-  //         $foto_lama = $data->fetch_assoc();
-  //         unlink('../resto-api/assets/img/makanan/' . $foto_lama['foto']);
-
-  //         $sql = "UPDATE makanan SET nama_makanan='$nama_makanan', harga_makanan='$harga_makanan', foto='$new_names' WHERE id='$id'";
-  //         $query = $db->query($sql);
-  //         if ($query === true) {
-
-  //           // pindahkan foto kedalam folder penyimpanan
-  //           move_uploaded_file($foto_tmp, '../resto-api/assets/img/makanan/' . $new_names);
-
-  //           $response = new Response("success", "", "Data berhasil diupdate", 200);
-  //           return $response->response_updateMakanan();
-  //         } else {
-
-  //           $response = new Response("success", "", "Data gagal diupdate", 400);
-  //           return $response->response_updateMakanan();;
-  //         }
-  //       } else {
-  //         $response = new Response("fail", "", "Gagal update makanan, semua field harus diisi!", 400);
-  //         return $response->response_field_empty();
-  //       }
-  //     } else {
-
-  //       $response = new Response("fail", "", "Gagal update makanan, id tidak ditemukan", 400);
-  //       return $response->response_idNotFound();
-  //     }
-  //   } else {
-
-  //     $sql = "SELECT * FROM makanan WHERE id='$id'";
-  //     $data = $db->query($sql);
-
-  //     if ($data->num_rows == 1) {
-
-  //       if ($nama_makanan !== '' && $harga_makanan !== '') {
-
-  //         $foto = $data->fetch_assoc();
-  //         $foto_lama = $foto['foto'];
-
-  //         $sql = "UPDATE makanan SET nama_makanan='$nama_makanan', harga_makanan='$harga_makanan', foto='$foto_lama' WHERE id='$id'";
-  //         $query = $db->query($sql);
-  //         if ($query === true) {
-
-  //           $response = new Response("success", "", "Data berhasil diupdate", 200);
-  //           return $response->response_updateMakanan();
-  //         } else {
-
-  //           $response = new Response("success", "", "Data gagal diupdate", 400);
-  //           return $response->response_updateMakanan();;
-  //         }
-  //       } else {
-  //         $response = new Response("fail", "", "Gagal update makanan, semua field harus diisi!", 400);
-  //         return $response->response_field_empty();
-  //       }
-  //     } else {
-
-  //       $response = new Response("fail", "", "Gagal update makanan, id tidak ditemukan", 400);
-  //       return $response->response_idNotFound();
-  //     }
-  //   }
-  // }
-
-  // public function deleteMakanan($id)
-  // {
-
-  //   if (!empty($id)) {
-
-  //     $sql = "SELECT * FROM makanan WHERE id='$id'";
-  //     $data = $db->query($sql);
-
-  //     if ($data->num_rows == 1) {
-
-  //       // menghapus foto dari direktori penyimpanan
-  //       $foto = $data->fetch_assoc();
-  //       unlink('../resto-api/assets/img/makanan/' . $foto['foto']);
-
-  //       // menghapus data dari database
-  //       $sql = "DELETE FROM makanan WHERE id='$id'";
-  //       $status = $db->query($sql);
-
-  //       if ($status === true) {
-
-  //         $response = new Response("success", "", "Data berhasil dihapus", 200);
-  //         return $response->response_deleteMakanan();
-  //       } else {
-
-  //         $response = new Response("fail", "", "Data gagal dihapus!", 400);
-  //         return $response->response_deleteMakanan();
-  //       }
-  //     } else {
-
-  //       $response = new Response("fail", "", "Data dengan id:{$id} tidak ditemukan", 400);
-  //       return $response->response_idNotFound();
-  //     }
-  //   } else {
-  //     $response = new Response("fail", "", "Data gagal dihapus, silahkan masukan id", 400);
-  //     return $response->response_idNotFound();
-  //   }
-  // }
+      $response = $this->helper->response_failAddMakanan("fail", "Data gagal ditambahkan, field tidak boleh kosong", 400);
+      return $response->response_field_empty();
+    }
+  }
 }
